769829248912b9cb210f46f803d2ff9460cc1a74
Andrew Kobus <kobusand@gmail.com>
2016-04-09 14:05:14 -0400

    Added validation

--- KabbageTest/scripts/KabbageTest_Main.js
+++ KabbageTest/scripts/KabbageTest_Main.js
@@ -13,6 +13,7 @@
     var annRev = "";
     var grossPerc = "";
     var year = "";
+    var error = "";
 
     //Retrieves data from textBoxes
     firstName = getTextboxValue("txtFirstName");
@@ -29,18 +30,31 @@
     type = getDropdownValue("cmbType");
 
     //For Testing
-    //firstName = "John";
-    //lastName = "Doe";
-    //email = "john.doe@company.com";
-    //busName = "Acme Inc.";
-    //phone = "800-555-1234";
-    //year = "2003";
-    //fico = "601";
-    //annRev = "99000";
-    //grossPerc = "85";
+    firstName = "John";
+    lastName = "Doe";
+    email = "john.doe@company.com";
+    busName = "Acme Inc.";
+    phone = "800-555-1234";
+    year = "2003";
+    fico = "601";
+    annRev = "99000";
+    grossPerc = "85";
     //type = "AutoRepair";
 
-    getKabbageQual(firstName, lastName, email, busName, phone, type, year, fico, annRev, grossPerc);
+    grossPerc = grossPerc.replace("%", "");
+    annRev = annRev.replace(",", "");
+    annRev = annRev.replace("$", "");
+    fico = fico.replace(",", "");
+    fico = fico.replace("$", "");
+
+    error = validateData(firstName, lastName, email, busName, phone, type, year, fico, annRev, grossPerc);
+
+    if (error == "") {
+        getKabbageQual(firstName, lastName, email, busName, phone, type, year, fico, annRev, grossPerc);
+    }
+    else {
+        alert(error);
+    }
 }
 
 function getTextboxValue(controlID) {
@@ -63,6 +77,48 @@ function getDropdownValue(controlID) {
     return value;
 }
 
+function validateData(firstName, lastName, email, busName, phone, type, year, fico, annRev, grossPerc) {
+    var text = "";
+
+    if (firstName == "") {
+        text = "Please enter a First Name";
+    }
+    else if (lastName == "") {
+        text = "Please enter a Last Name";
+    }
+    else if (email == "") {
+        text = "Please enter an email address";
+    }
+    else if (email.search("@") == -1) {
+        text = "Your email address is incorrect";
+    }
+    else if (busName == "") {
+        text = "Your business name is invalid";
+    }
+    else if (phone == "") {
+        text = "Your phone number is incorrect";
+    }
+    else if (type == "ChooseaTypeofBusiness") {
+        text = "Please select a business type";
+    }
+    else if (year == "" || isNaN(parseFloat(year))) {
+        text = "The year you've entered in is incorrect";
+    }
+    else if (fico == "" || isNaN(parseFloat(fico))) {
+        text = "Your FICO amount is incorrect";
+    }
+    else if (annRev == "" || isNaN(parseFloat(annRev))) {
+        text = "Your annual revenue is incorrect";
+    }
+    else if (grossPerc == "" || isNaN(parseFloat(grossPerc))) {
+        text = "Your gross revenue from cards is incorrect";
+    }
+
+    return text;
+}
+
 function getKabbageQual(firstName, lastName, email, busName, phone, type, year, fico, annRev, grossPerc) {
 
 }
+
+
